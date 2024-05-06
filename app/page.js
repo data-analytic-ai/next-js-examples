7c4dd90b7d244276c6d6a2c47826802c1e809723
Jocsan Mairena <jocsanmairena@users.noreply.github.com>
2024-05-05 21:46:21 -0400

    Inside Page Component and initial clean up

--- app/page.js
+++ app/page.js
@@ -1,41 +1,6 @@
-'use client'
-
-import {useState} from "react"
-
-function Card({children}) {
-    return (
-        <div className="border rounded-md border-gray-600 p-4">
-            {children}
-        </div>
-    )
-}
 
 export default function Home() {
-    const [isVisible, setIsVisible] = useState(true)
-
-    const handleClick = () => {
-        setIsVisible(!isVisible)
-    }
-    const cards = isVisible &&
-        <>
-            <Card>This is being passed from the parent</Card>
-            <Card>
-                <div>This is JS!</div>
-                <Card>Nested text!</Card>
-            </Card>
-            <Card/>
-            <Card/>
-            <Card/>
-        </>
     return (
-        <>
-            <div className="p-20 space-y-4">
-                <div>Hello, {name}</div>
-                {cards}
-                <button onClick={handleClick}>
-                    {isVisible ? 'Hide' : 'Show'}
-                </button>
-            </div>
-        </>
+        <>Hello Baby!</>
     )
 }
